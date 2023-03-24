c569729035f82300871152e471dd5d9f9b3ca077
BOZHI WANG <wbz13204977@gmail.com>
2023-03-24 14:00:34 -0400

    Catalog Feature
    figure out the catalog

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,7 +1,9 @@
+import data from "./data";
+
 function Products(){
     return (
-        <div>
-            <h2>Products</h2>
+        <div className="content">
+            <ul className="products"></ul>
         </div>
     );
 }
