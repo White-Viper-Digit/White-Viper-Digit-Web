72839f90549c02c930b39bd99398f48db26c6be0
BOZHI WANG <123031435+KKcoo1320@users.noreply.github.com>
2023-04-18 16:12:37 -0400

    Update App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main app', () => {
+  test('should render the Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
 });
+
+it('should render the Home link', () => {
+  render (<App />);
+  expect(screen.getByRole ('link', { name: "Home"} )) . toßeInTheDocument();
+  });
+  
+it('should render the Home header', () => {
+  render (<App />);
+  expect(screen.getByRole ('heading', { level: 2} )) . toßeInTheDocument();
+  });
