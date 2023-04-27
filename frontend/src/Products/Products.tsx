b240f37030d235df8c482307c946bee863926708
BOZHI WANG <123031435+KKcoo1320@users.noreply.github.com>
2023-04-27 17:03:49 -0400

    Update Products.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,34 +1,62 @@
-import data from "./data";
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numberOfReviews: number;
+  imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
 
 function Products(){
-    return (
-        <div className="content">
-            <ul className="products">
-            <ul className="products">
-                  {data.products.map((product) => (
-                    <li>
-                        <div className="product">
-                          <img
-                          className="product-image"
-                          src={product.imageUrl}
-                          alt="product"
-                          />
-                          <div className="product-name">
-                            <a href="product.html">{product.name}</a>
-                            </div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">{product.price}</div>
-                            <div className="product-rating">
-                              {product.rating} Stars ({product.numberOfReviews} reviews)
-                            </div>
-                          </div>
-                         </li>
-                  ))}
-                  ;
-                  </ul>
-            </ul>
-        </div>
-    );
+
+  const [products, setProducts]: [Product[], (products: Product[])=> void]
+  = useState(emptyProducts);
+
+  useEffect(() => {
+    axios.get<Product[]>("https://localhost:7250/catalog",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => setProducts(response.data))
+    .catch((error) => console.log(error));
+  },[]);
+  return (
+    <div className="content">
+        <ul className="products">
+        <ul className="products">
+              {products.map((product) => (
+                <li key={product.id}>
+                    <div className="product">
+                      <img
+                      className="product-image"
+                      src={product.imageUrl}
+                      alt="product"
+                      />
+                      <div className="product-name">
+                        <a href="product.html">{product.name}</a>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">{product.price}</div>
+                        <div className="product-rating">
+                          {product.rating} Stars ({product.numberOfReviews} reviews)
+                        </div>
+                      </div>
+                     </li>
+              ))}
+              ;
+              </ul>
+        </ul>
+    </div>
+);
 }
 
 export default Products;
